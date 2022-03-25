51097172821ac94c031a3325dc4447a94d33564e
rezagi <rezagi66@gmail.com>
2022-03-25 22:02:54 +0700

    first commit

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import { PORT } from "./config.js";
-import product_routes from "./routes/product_route.js";
 import school_routes from "./routes/school_route.js";
 import student_routes from "./routes/student_route.js";
 
@@ -14,7 +13,6 @@ const error_handling = (err, req, res, next) => {
 
 app.use(error_handling);
 app.use(bodyParser.json({ type: "application/json" }));
-app.use("/products", product_routes);
 app.use("/schools", school_routes);
 app.use("/students", student_routes);
 
